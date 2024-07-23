7739eef62c9cb5de2aa70449c588aa9786687118
Umair Shahid <umair.shahid@live.ca>
2024-07-23 14:37:19 +0300

    added custom css and form

--- include/form.php
+++ include/form.php
@@ -27,8 +27,8 @@ require 'phpmailer/src/SMTP.php';
 $toemails = array();
 
 $toemails[] = array(
-	'email' => 'your-email@website.com', // Your Email Address
-	'name' => 'Your Name' // Your Name
+	'email' => 'ushahid@sanam.com', // Your Email Address
+	'name' => 'Umair Shahid' // Your Name
 );
 
 
@@ -37,7 +37,7 @@ $toemails[] = array(
 ---------------------------------------------------*/
 
 $fromemail = array(
-	'email' => 'no-reply@website.com', // Company's Email Address (preferably currently used Domain Name)
+	'email' => '', // Company's Email Address (preferably currently used Domain Name)
 	'name' => 'Company Name' // Company Name
 );
 
